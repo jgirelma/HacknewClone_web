b9c388793bc2b1af4ea221e845cab67c3c553dac
Jack <jgirelma@andrew.cmu.edu>
2020-07-30 17:17:10 -0400

    removed uneeded bottom

--- src/Pages/NewPost.tsx
+++ src/Pages/NewPost.tsx
@@ -67,9 +67,6 @@ export default function NewPost() {
               </Form>
             )}
           </Formik>
-          <p className="text-center text-gray-500 text-xs">
-            &copy;2020 Acme Corp. All rights reserved.
-          </p>
         </div>
       </div>
     );
