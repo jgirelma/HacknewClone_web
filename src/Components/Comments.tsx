8a5be6d5fc9e8a83f18073c0f90cd4aa6f38c3d1
Jack <jgirelma@andrew.cmu.edu>
2020-08-02 23:39:09 -0400

    Removed react title, icon

--- src/Components/Comments.tsx
+++ src/Components/Comments.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import Comment from "./Comment";
 import { SortSelectorForComments } from "./SortSelector";
 import CommentsContext from "../Contexts/CommentsContext";
